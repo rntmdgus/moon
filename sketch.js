4b052cdb28ee99574c54c6217f4626dedd6d6e19
rntmdgus <rntmdgus@naver.com>
2014-11-06 15:55:18 +0900

    3454354
    353535

--- sketch.js
+++ sketch.js
@@ -33,15 +33,15 @@ function draw() {
 	stroke(255);
 	ellipse(500 + Math.cos(timing1*2*PI)*300,
         500 + Math.sin(timing1*2*PI)*300,
-        80,
-        350);
+        100,
+        300);
 
 	//달타원라인2
 	noFill();
 	stroke(255);
 	ellipse(500 + Math.cos(timing1*2*PI)*300,
         500 + Math.sin(timing1*2*PI)*300,
-        200,
+        250,
         150);
 
 	//달라인
@@ -57,7 +57,7 @@ function draw() {
 
 
  	//달1
-	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*100-100,
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*80-100,
 		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing2*4*PI)*50-100,200,200);
 
 	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing3*4*PI)*100-100,
