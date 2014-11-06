57dbda1dc2cb8ae5d24eafe6d843c5dc14e69091
rntmdgus <rntmdgus@naver.com>
2014-11-06 15:33:52 +0900

    4314
    4134

--- sketch.js
+++ sketch.js
@@ -1,12 +1,14 @@
 var solar;
 var bg;
 var earth;
+var moon;
 
 function setup() {
     createCanvas(1000, 1000);
 	solar = loadImage("sun.png");
 	bg = loadImage("bg.png");
 	earth = loadImage("earth.png");
+	moon = loadImage("moon.png");
 }
 
 
@@ -55,26 +57,12 @@ function draw() {
 	image(earth,500 + Math.cos(timing1*2*PI)*300-150,500 + Math.sin(timing1*2*PI)*300-150,300,300);
 
  	//달1
-	fill(196,158,0);
-	stroke(196,158,0);
-	ellipse(500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*100,           // x좌표
-        500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing2*4*PI)*100,		 // y좌표
-        20,                                	  // width
-        20); 
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*100-100,
+		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing2*4*PI)*100-100,200,200);
 
- 	//달2
-	fill(196,158,0);
-	stroke(196,158,0);
-	ellipse(500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing3*4*PI)*100,           // x좌표
-        500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing3*4*PI)*100,		 // y좌표
-        30,                                	  // width
-        30); 
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing3*4*PI)*100-100,
+		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing3*4*PI)*100-100,200,200);
 
-	//달3
-	fill(196,158,0);
-	stroke(196,158,0);
-	ellipse(500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing4*4*PI)*100,           // x좌표
-        500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing4*4*PI)*100,		 // y좌표
-        40,                                	  // width
-        40); 
+	image(moon,500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing4*4*PI)*100-100,
+		500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing4*4*PI)*100-100,200,200);
 }
