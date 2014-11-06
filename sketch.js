610944f5d789322f41f6e43dcb15489cba5bd8c0
rntmdgus <rntmdgus@naver.com>
2014-11-06 15:24:58 +0900

    324

--- sketch.js
+++ sketch.js
@@ -52,29 +52,29 @@ function draw() {
 
 
  	//지구
-	image(earth,600 + Math.cos(timing1*2*PI)*250-150,600 + Math.sin(timing1*2*PI)*250-150,300,300);
+	image(earth,500 + Math.cos(timing1*2*PI)*300-150,500 + Math.sin(timing1*2*PI)*300-150,300,300);
 
  	//달1
 	fill(196,158,0);
 	stroke(196,158,0);
-	ellipse(600 + Math.cos(timing1*2*PI)*250 + Math.cos(timing2*4*PI)*100,           // x좌표
-        600 + Math.sin(timing1*2*PI)*250 + Math.sin(timing2*4*PI)*100,		 // y좌표
+	ellipse(500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing2*4*PI)*100,           // x좌표
+        500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing2*4*PI)*100,		 // y좌표
         20,                                	  // width
         20); 
 
  	//달2
 	fill(196,158,0);
 	stroke(196,158,0);
-	ellipse(600 + Math.cos(timing1*2*PI)*250 + Math.cos(timing3*4*PI)*100,           // x좌표
-        600 + Math.sin(timing1*2*PI)*250 + Math.sin(timing3*4*PI)*100,		 // y좌표
+	ellipse(500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing3*4*PI)*100,           // x좌표
+        500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing3*4*PI)*100,		 // y좌표
         30,                                	  // width
         30); 
 
 	//달3
 	fill(196,158,0);
 	stroke(196,158,0);
-	ellipse(600 + Math.cos(timing1*2*PI)*250 + Math.cos(timing4*4*PI)*100,           // x좌표
-        600 + Math.sin(timing1*2*PI)*250 + Math.sin(timing4*4*PI)*100,		 // y좌표
+	ellipse(500 + Math.cos(timing1*2*PI)*300 + Math.cos(timing4*4*PI)*100,           // x좌표
+        500 + Math.sin(timing1*2*PI)*300 + Math.sin(timing4*4*PI)*100,		 // y좌표
         40,                                	  // width
         40); 
 }
